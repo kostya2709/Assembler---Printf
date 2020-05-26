65afa451f36dc2d6721b3cd50c097a9691d53148
Dragun Konstantin <k.dragun.k@mail.ru>
2020-05-26 14:05:58 +0300

    Update printf.asm

--- printf.asm
+++ printf.asm
@@ -81,13 +81,13 @@ section .text
 %endmacro
 
 %macro write_buf 1
-    mov byte [rdi], %1						; Move the necessary character to the destination address.
-    inc rdi								    ; Increment the destination register.
-    add dl, 1                               ; Increment the printf_buffer counter.
-    jnc %%next                              ; If overflow is not detected, move further.
-    ;puts "THE BUFFER IS FULL"              ; To test how buffer overflow control works.
-    call _write_result                      ; If overflow is detected, let's write down the buffer.
-%%next:                                     ; If overflow is not detected, move further.
+    mov byte [rdi], %1			; Move the necessary character to the destination address.
+    inc rdi				; Increment the destination register.
+    add dl, 1                           ; Increment the printf_buffer counter.
+    jnc %%next                          ; If overflow is not detected, move further.
+    ;puts "THE BUFFER IS FULL"          ; To test how buffer overflow control works.
+    call _write_result              	; If overflow is detected, let's write down the buffer.
+%%next:                                 ; If overflow is not detected, move further.
 %endmacro
 
 
@@ -98,7 +98,7 @@ _check_canary:                              ; This function checks whether the b
     je .end
     puts "Error! The memory is damaged!\n"
     mov rax, 1		                        ; System call "sys_exit"
-	mov rbx, 0		                        ; Error number
+	mov rbx, 0		                ; Error number
 	int 0x80
 .end:    
     pop rax
@@ -180,12 +180,12 @@ _printf:
     mov r10, printf_buffer
 
     xor r11, r11                		; Counter of the written variables.
-    xor rdx, rdx                        ; Counter of symbols in the printf_buffer.
+    xor rdx, rdx                        	; Counter of symbols in the printf_buffer.
 
     mov rsi, [cur_arg]            		; RSI = pointer to the format string.
     mov rdi, printf_buffer        		; RDI = pointer to the buffer where to write symbols to.
 
-.Printf_Cycle:                	    	; The main cycle of the _printf function.
+.Printf_Cycle:                	    		; The main cycle of the _printf function.
 
     mov al, [rsi]                		; al = symbol in format string.
     cmp al, 0                    		; Comparison al with the terminating symbol of the string.
@@ -198,7 +198,7 @@ _printf:
 
 .next_form:                        		; Let's check, whether the symbol is slash.
     cmp al, 5ch                    		; Comparison
-    jne .Printf_Sym              	; If not, just print the symbol at last.
+    jne .Printf_Sym              		; If not, just print the symbol at last.
     call _found_slash            		; If yes, let's call the function and handle the slash.
     jmp .Printf_Cycle            		; Having done everything necessary, let's return to the main cycle.
     
@@ -209,7 +209,7 @@ _printf:
     jmp .Printf_Cycle            		; Jump to the main cycle.
     
     
-Printf_End:                				; We jump to this label, if we encountered the terminating symbol.
+Printf_End:                			; We jump to this label, if we encountered the terminating symbol.
 
     call _write_result
 
@@ -227,7 +227,7 @@ _write_result:
     mov rbx, STD_OUT                	; The file descriptor "stdout".
     mov rcx, printf_buffer          	; Pointer to the buffer.
     mov rdx, rdi                    	; RDX = RDI - RAX = number of symbols written into the buffer.
-	int 0x80                            ; Call the interruption.
+	int 0x80                        ; Call the interruption.
 
     call _check_canary
 
@@ -239,8 +239,8 @@ ret
 
 
 
-										; There are some useful defines which are used below
-                                    	; as arguments in the macros.
+							; There are some useful defines which are used below
+                                    			; as arguments in the macros.
 %define is_long     	r14				; The parameter of length in Printf_Digital auxilary function.
 %define is_unsgn     	r15				; The parameter of sign in Printf_Digital auxilary function.
 %define is_ptr        	r14				; The parameter of whether the function Printf_Hex is called to write a pointer.
@@ -248,44 +248,44 @@ ret
 %define is_capital      r14				; The parameter of using capital letters in Printf_Hex auxilary function.
 
 
-;╔═══════════════════════════════════_found_format══════════════════════════════════════╗
-;║ This function is used to detect special format, following percent.                   ║
-;║ In case if no match was found, the symbol which follows percent, is skipped.         ║
-;║                                                                                      ║
-;║	ARGUMENTS: some registers are used for arguments in the auxilary functions:         ║
-;║					r14 - for being long, being a pointer, or for writting with         ║
-;║							capital letters. 											║
-;║					r15 - for being unsigned, or to be the necessary degree of two.     ║
-;╚══════════════════════════════════════════════════════════════════════════════════════╝
+;╔═══════════════════════════════════_found_format═════════════════════════════════════════════════════════╗
+;║ This function is used to detect special format, following percent.                                      ║
+;║ In case if no match was found, the symbol which follows percent, is skipped.                            ║
+;║                                                                                                         ║
+;║	ARGUMENTS: some registers are used for arguments in the auxilary functions:                        ║
+;║					r14 - for being long, being a pointer, or for writting with        ║
+;║							capital letters. 				   ║							║
+;║					r15 - for being unsigned, or to be the necessary degree of two.    ║
+;╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╝
 _found_format:
     
     inc rsi  
-    xor rbx, rbx                       	; So, '%' was found. Let's check what is next.
+    xor rbx, rbx                       		; So, '%' was found. Let's check what is next.
     mov bl, byte [rsi]
 
 ;    jecall 'c', _Printf_Char			; Function for a char is called.
 ;    jecall 's', _Printf_String			; Function for a string is called.
-;    jecall '%', _Printf_Percent			; Function for a percent is called.
+;    jecall '%', _Printf_Percent		; Function for a percent is called.
     
-										; In the next lines: first argument - r14, the second one - r15
-;    jecall 'd', _Printf_Digit, 0, 0 	; Function for a 32-bits signed digit is called.
+						; In the next lines: first argument - r14, the second one - r15
+;    jecall 'd', _Printf_Digit, 0, 0 		; Function for a 32-bits signed digit is called.
 ;    jecall 'u', _Printf_Digit, 0, 1		; Function for a 32-bits unsigned digit is called.
 ;    jecall 'l', _Printf_Digit, 1, 0		; Function for a 64-bits signed digit is called.
 ;    jecall 'v', _Printf_Digit, 1, 1		; Function for a 64-bits unsigned digit is called.
     
-										; In the next lines numbers are inverted in a particular
-										; system based on degree of 2.
+						; In the next lines numbers are inverted in a particular
+						; system based on degree of 2.
 ;    jecall 'b', _Printf_Hex, 0, 1		; Function for a binary format (2 ^ 1) is called.
 ;    jecall 'q', _Printf_Hex, 0, 2		; Function for a quaternary format (2 ^ 2) is called.
 ;    jecall 'o', _Printf_Hex, 0, 3		; Function for an octal format (2 ^ 3) is called.
-;    jecall 'h', _Printf_Hex, 0, 4       ; Function for a hex format (2 ^ 4) is called.
-;    jecall 'x', _Printf_Hex, 26d, 4     ; Function for a hex format (2 ^ 4) with capital letters is called.
-;    jecall 'p', _Printf_Hex, 1, 4       ; Function for a pointer format (2 ^ 4) is called.
-;    jecall 'f', _Printf_Hex, 0, 5       ; Function for a 32-nd format (2 ^ 5) is called.
-;    jecall 'i', _Printf_Hex, 0, 6       ; Function for a 64-th format (2 ^ 6) is called.
+;    jecall 'h', _Printf_Hex, 0, 4       	; Function for a hex format (2 ^ 4) is called.
+;    jecall 'x', _Printf_Hex, 26d, 4     	; Function for a hex format (2 ^ 4) with capital letters is called.
+;    jecall 'p', _Printf_Hex, 1, 4       	; Function for a pointer format (2 ^ 4) is called.
+;    jecall 'f', _Printf_Hex, 0, 5       	; Function for a 32-nd format (2 ^ 5) is called.
+;    jecall 'i', _Printf_Hex, 0, 6       	; Function for a 64-th format (2 ^ 6) is called.
 
 
-    jecall '%', _Printf_Percent		    ; Function for a percent is called.
+    jecall '%', _Printf_Percent		; Function for a percent is called.
 
     sub bl, 'a'                         ; Get value in range [0, 26]
     jmp [.Jump_Table + rbx * 8]         ; Jump to the adress in Jump_Table
@@ -317,13 +317,13 @@ _found_format:
 
 
 .handle_b:
-    jmpcall _Printf_Hex, 0, 1		    ; Function for a binary format (2 ^ 1) is called.
+    jmpcall _Printf_Hex, 0, 1		; Function for a binary format (2 ^ 1) is called.
 
 .handle_c:
-    jmpcall _Printf_Char			    ; Function for a char is called.
+    jmpcall _Printf_Char		; Function for a char is called.
 
 .handle_d:
-    jmpcall _Printf_Digit, 0, 0 	    ; Function for a 32-bits signed digit is called.
+    jmpcall _Printf_Digit, 0, 0 	; Function for a 32-bits signed digit is called.
 
 .handle_f:
     jmpcall _Printf_Hex, 0, 5           ; Function for a 32-nd format (2 ^ 5) is called.
@@ -335,43 +335,43 @@ _found_format:
     jmpcall _Printf_Hex, 26d, 4         ; Function for a hex format (2 ^ 4) with capital letters is called.
 
 .handle_l:
-    jmpcall _Printf_Digit, 1, 0		    ; Function for a 64-bits signed digit is called.
+    jmpcall _Printf_Digit, 1, 0		; Function for a 64-bits signed digit is called.
 
 .handle_o:
-    jmpcall _Printf_Hex, 0, 3		    ; Function for an octal format (2 ^ 3) is called.
+    jmpcall _Printf_Hex, 0, 3		; Function for an octal format (2 ^ 3) is called.
 
 .handle_p:
     jmpcall _Printf_Hex, 1, 4           ; Function for a pointer format (2 ^ 4) is called.
 
 .handle_q:
-    jmpcall _Printf_Hex, 0, 2		    ; Function for a quaternary format (2 ^ 2) is called.
+    jmpcall _Printf_Hex, 0, 2		; Function for a quaternary format (2 ^ 2) is called.
 
 .handle_s:
-    jmpcall _Printf_String			    ; Function for a string is called.
+    jmpcall _Printf_String		; Function for a string is called.
 
 .handle_u:
-    jmpcall _Printf_Digit, 0, 1		    ; Function for a 32-bits unsigned digit is called.
+    jmpcall _Printf_Digit, 0, 1		; Function for a 32-bits unsigned digit is called.
 
 .handle_v:
-    jmpcall _Printf_Digit, 1, 1		    ; Function for a 64-bits unsigned digit is called.
+    jmpcall _Printf_Digit, 1, 1		; Function for a 64-bits unsigned digit is called.
 
 .handle_x:
     jmpcall _Printf_Hex, 0, 4           ; Function for a hex format (2 ^ 4) is called.
 
-_format_endian:							; Label of end of the format switch.
+_format_endian:				; Label of end of the format switch.
 .End:
     inc rsi                            	; Let's skip the symbol after '%'.
-ret										; Leave the switch.
+ret					; Leave the switch.
 
 
 ;╔═══════════════════════════════════_Printf_Char══════════════════════════════════════╗
-;║	This function writes a char.                                                       ║
-;║	ARGUMENTS: 		cur_arg - character to write.                                      ║
-;║					rdi     - where to write to.                                       ║
+;║	This function writes a char.                                                   ║
+;║	ARGUMENTS: 		cur_arg - character to write.                          ║
+;║					rdi     - where to write to.                   ║
 ;╚═════════════════════════════════════════════════════════════════════════════════════╝
 _Printf_Char:
 
-    inc arg_count						; Increment the number of the current argument in the stack.
+    inc arg_count					; Increment the number of the current argument in the stack.
     mov bl, [cur_arg]					; Move the necessary character to register bl.
     write_buf bl
 ret
